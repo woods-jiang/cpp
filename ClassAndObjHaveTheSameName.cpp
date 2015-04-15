7f6b19d61f2163b18f1e4441cb3289eb19b5519d
Woods Jiang <woods.jiang.1@gmail.com>
2015-04-15 22:22:38 +0800

    未解决问题

---
+++ ClassAndObjHaveTheSameName.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	A()
+	{
+		data = 10;
+	}
+	int data;
+};
+
+int main()
+{
+	A A;
+
+	cout << A.data << endl;
+
+//	A theA;
+	
+	return 0;
+}
